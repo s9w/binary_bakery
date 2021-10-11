6ed396353e6ab465f866cf2966c77869c7eeac0f
Sebastian Werhausen <swerhausen@gmail.com>
2021-10-11 19:58:10 +0200

    added measuring code for compile time and resulting binary size

---
+++ compile_metrics/compile.ps1
@@ -0,0 +1,63 @@
+function Invoke-CmdScript {
+   param(
+      [String] $script_path
+   )
+   Write-Output "running script $script_path"
+   $cmdLine = """$script_path"" $args & set"
+   & $Env:SystemRoot\system32\cmd.exe /c $cmdLine |
+   select-string '^([^=]*)=(.*)$' | foreach-object {
+      $varName = $_.Matches[0].Groups[1].Value
+      $varValue = $_.Matches[0].Groups[2].Value
+      set-item Env:$varName $varValue
+   }
+}
+
+# $vs_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
+$vs_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional"
+$vcvars_dir = "{0}\VC\Auxiliary\Build\vcvars64.bat" -f $vs_dir
+
+if((-Not (Test-Path env:cvars_invoked)) -And (-Not $env:cvars_invoked)){
+   Invoke-CmdScript -script_path $vcvars_dir
+   # prevent running that script more than once per session. It's slow and there's an issue with multiple invokations
+   $env:cvars_invoked = $true
+}
+
+$cl_command = "CL /O2 /MDd /D _DEBUG /std:c++latest /EHsc /nologo /permissive- /W4 /wd4189 /utf-8 /Feout.exe source.cpp /link /MACHINE:X64"
+$cl_cmd_zero = "CL /O2 /MDd /D _DEBUG /D ZERO /std:c++latest /EHsc /nologo /permissive- /W4 /wd4189 /utf-8 /Feout.exe source.cpp /link /MACHINE:X64"
+
+# Measure zero includes
+Invoke-Expression $cl_cmd_zero
+$size=(Get-Item out.exe).length
+Write-Output ("{0}, {1}" -f 0, $size) | Out-File -FilePath ("compile_sizes.txt") -Append -Encoding ASCII
+Remove-Item -Path *.obj
+Remove-Item -Path out.exe
+
+For ($i=0; $i -lt 5; $i++){
+   Measure-Command {
+      Invoke-Expression $cl_cmd_zero
+   } | Out-File -FilePath ("compile_time_{0}.txt" -f 0) -Append -Encoding ASCII
+   Remove-Item -Path *.obj
+   Remove-Item -Path out.exe
+}
+
+Get-ChildItem "generating" -Filter *.png | Foreach-Object{
+   $expr = "generating/binary_bakery.exe {0}" -f $_.FullName
+   Invoke-Expression $expr
+
+   # One try first, check for errors and report size
+   Invoke-Expression $cl_command
+   $size=(Get-Item out.exe).length
+   Write-Output ("{0}, {1}" -f $_.Basename, $size) | Out-File -FilePath ("compile_sizes.txt") -Append -Encoding ASCII
+
+   Remove-Item -Path *.obj
+   Remove-Item -Path out.exe
+
+   # Measure compile time multiple times
+   For ($i=0; $i -lt 5; $i++){
+      Measure-Command {
+         Invoke-Expression $cl_command
+      } | Out-File -FilePath ("compile_time_{0}.txt" -f $_.BaseName) -Append -Encoding ASCII
+      Remove-Item -Path *.obj
+      Remove-Item -Path out.exe
+   }
+}
---
+++ compile_metrics/compile_metrics_plot.py
@@ -0,0 +1,54 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from os.path import isfile, join
+from matplotlib import ticker
+import re
+
+def get_times_from_txt(filename):
+    ms_regex = re.compile(r'TotalMilliseconds\s?:\s?(\d*[,.]\d*)')
+    with open(filename) as file:
+        times = np.zeros(shape=(0))
+        for line in file.readlines():
+            match = ms_regex.match(line.strip())
+            if match is None:
+                continue
+            ms_str = match.group(1).replace(",", ".")
+            ms = float(ms_str)
+            seconds = ms / 1000.0
+            times = np.append(times, seconds)
+        return times[1:]
+
+data = np.loadtxt("compile_sizes.txt", dtype=int, delimiter=',', usecols=[0, 1], unpack=False)
+data = data[data[:, 0].argsort()] # Sort by size
+
+source_sizes_kb = data[:,0] / 1024
+binary_sizes_kb = data[:,1] / 1024
+binary_size_overhead = binary_sizes_kb - binary_sizes_kb[0] - source_sizes_kb
+
+compile_times = [np.mean(get_times_from_txt("compile_time_{}.txt".format(ss))) for ss in data[:,0]]
+compile_times_increases = compile_times - compile_times[0]
+
+fig = plt.figure(figsize=(6, 3))
+ax = fig.add_subplot(111)
+y = compile_times - compile_times[0]
+ax.plot(source_sizes_kb, binary_size_overhead, "o--")
+ax.set_xscale('log')
+ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x:8.0f}"))
+ax.set_ylim(0, 2.5)
+ax.set_xlabel("Memory size of include [KB]")
+ax.set_ylabel("Binary size overhead [KB]")
+fig.tight_layout()
+fig.savefig("binary_sizes.png")
+
+fig = plt.figure(figsize=(6, 3))
+ax = fig.add_subplot(111)
+ax.plot(source_sizes_kb, compile_times_increases, "o-")
+ax.set_xscale('log')
+ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x:8.0f}"))
+ax.set_xlabel("Memory size of include [KB]")
+ax.set_ylabel("Compile time increase [seconds]")
+ax.set_ylim(0, )
+fig.tight_layout()
+fig.savefig("compile_times.png")
