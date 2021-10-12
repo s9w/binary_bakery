5122ade721966247f5354f6a202db44260d71efb
Sebastian Werhausen <swerhausen@gmail.com>
2021-10-12 20:37:45 +0200

    config tests

--- tests/config_tests.cpp
+++ tests/config_tests.cpp
@@ -5,9 +5,17 @@
 using namespace bb;
 
 
-TEST_CASE("config files")
+TEST_CASE("get_cfg_from_dir()")
 {
    const auto cfg = get_cfg_from_dir(abs_directory_path{ "test_configs" }).value();
+   CHECK_EQ(cfg.output_filename, "123.h");
+   CHECK_EQ(cfg.smart_mode, false);
+   CHECK_EQ(cfg.compression, compression_mode::zstd);
+}
+
+TEST_CASE("get_cfg_from_file()")
+{
+   const auto cfg = get_cfg_from_file(abs_file_path{ "test_configs/c0.toml" }).value();
    CHECK_EQ(cfg.output_filename, "123.h");
    CHECK_EQ(cfg.smart_mode, false);
    CHECK_EQ(cfg.compression, compression_mode::zstd);
