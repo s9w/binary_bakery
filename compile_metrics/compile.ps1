68d7558a79033b496f3df3808cb814dae8f1e260
Sebastian Werhausen <swerhausen@gmail.com>
2021-10-12 20:18:51 +0200

    improved compile metric generation and scope

--- compile_metrics/compile.ps1
+++ compile_metrics/compile.ps1
@@ -12,8 +12,8 @@ function Invoke-CmdScript {
    }
 }
 
-$vs_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
-# $vs_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional"
+# $vs_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
+$vs_dir = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional"
 $vcvars_dir = "{0}\VC\Auxiliary\Build\vcvars64.bat" -f $vs_dir
 
 if((-Not (Test-Path env:cvars_invoked)) -And (-Not $env:cvars_invoked)){
@@ -26,41 +26,48 @@ $cl_command = "CL /O2 /MDd /D _DEBUG /std:c++latest /EHsc /nologo /permissive- /
 $cl_cmd_zero = "CL /O2 /MDd /D _DEBUG /D ZERO /std:c++latest /EHsc /nologo /permissive- /W4 /wd4189 /utf-8 /Feout.exe source.cpp /link /MACHINE:X64"
 
 # Measure zero includes
-Invoke-Expression $cl_cmd_zero
-$size=(Get-Item out.exe).length
-Write-Output ("{0}, {1}" -f 0, $size) | Out-File -FilePath ("compile_sizes.txt") -Append -Encoding ASCII
-Remove-Item -Path *.obj
-Remove-Item -Path out.exe
+# Invoke-Expression $cl_cmd_zero
+# $size=(Get-Item out.exe).length
+# Write-Output ("{0}, {1}" -f 0, $size) | Out-File -FilePath ("compile_sizes.txt") -Append -Encoding ASCII
+# Remove-Item -Path *.obj
+# Remove-Item -Path out.exe
 
-For ($i=0; $i -lt 5; $i++){
-   Start-Sleep -Seconds 0.2
-   Measure-Command {
-      Invoke-Expression $cl_cmd_zero
-   } | Out-File -FilePath ("compile_time_{0}.txt" -f 0) -Append -Encoding ASCII
-   Remove-Item -Path *.obj
-   Remove-Item -Path out.exe
-}
+# For ($i=0; $i -lt 5; $i++){
+#    Start-Sleep -Seconds 0.2
+#    Measure-Command {
+#       Invoke-Expression $cl_cmd_zero
+#    } | Out-File -FilePath ("compile_time_{0}.txt" -f 0) -Append -Encoding ASCII
+#    Remove-Item -Path *.obj
+#    Remove-Item -Path out.exe
+# }
 
-Get-ChildItem "generating" -Filter *.png | Foreach-Object{
-   $expr = "generating/binary_bakery.exe {0}" -f $_.FullName
-   Invoke-Expression $expr
-   Start-Sleep -Seconds 2.0
+$compressions = "none","zstd","lz4"
 
-   # One try first, check for errors and report size
-   Invoke-Expression $cl_command
-   $size=(Get-Item out.exe).length
-   Write-Output ("{0}, {1}" -f $_.Basename, $size) | Out-File -FilePath ("compile_sizes.txt") -Append -Encoding ASCII
+Foreach($compression in $compressions){
 
-   Remove-Item -Path *.obj
-   Remove-Item -Path out.exe
+   Get-ChildItem "..\sample_datasets" -Filter *.png | Foreach-Object{
+      $config_path = "config_{0}.toml" -f $compression
+      $expr = "../x64/Release/binary_bakery.exe {0} {1}" -f $config_path, $_.FullName
+      Invoke-Expression $expr
+      Start-Sleep -Seconds 1.0
 
-   # Measure compile time multiple times
-   For ($i=0; $i -lt 5; $i++){
-      Start-Sleep -Seconds 0.2
-      Measure-Command {
-         Invoke-Expression $cl_command
-      } | Out-File -FilePath ("compile_time_{0}.txt" -f $_.BaseName) -Append -Encoding ASCII
-      Remove-Item -Path *.obj
-      Remove-Item -Path out.exe
-   }
-}
+      # One try first, check for errors and report size
+   #    Invoke-Expression $cl_command
+   #    $size=(Get-Item out.exe).length
+   #    Write-Output ("{0}, {1}" -f $_.Basename, $size) | Out-File -FilePath ("compile_sizes.txt") -Append -Encoding ASCII
+
+   #    Remove-Item -Path *.obj
+   #    Remove-Item -Path out.exe
+
+   #    # Measure compile time multiple times
+   #    For ($i=0; $i -lt 5; $i++){
+   #       Start-Sleep -Seconds 0.2
+   #       Measure-Command {
+   #          Invoke-Expression $cl_command
+   #       } | Out-File -FilePath ("compile_time_{0}.txt" -f $_.BaseName) -Append -Encoding ASCII
+   #       Remove-Item -Path *.obj
+   #       Remove-Item -Path out.exe
+   #    }
+
+   } # sample_datasets
+} # compressions
--- compile_metrics/compile_metrics_plot.py
+++ compile_metrics/compile_metrics_plot.py
@@ -28,6 +28,7 @@ binary_sizes_kb = data[:,1] / 1024
 binary_size_overhead = binary_sizes_kb - binary_sizes_kb[0] - source_sizes_kb
 
 compile_times = [np.mean(get_times_from_txt("compile_time_{}.txt".format(ss))) for ss in data[:,0]]
+compile_times_std = [np.std(get_times_from_txt("compile_time_{}.txt".format(ss))) for ss in data[:,0]]
 compile_times_increases = compile_times - compile_times[0]
 
 fig = plt.figure(figsize=(6, 3))
@@ -47,7 +48,7 @@ fig.savefig("binary_sizes.png")
 
 fig = plt.figure(figsize=(6, 3))
 ax = fig.add_subplot(111)
-ax.plot(source_sizes_kb, compile_times_increases, "o--")
+ax.errorbar(source_sizes_kb, compile_times_increases, fmt="o--", yerr=compile_times_std)
 ax.set_xscale('log')
 ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x:8.0f}"))
 ax.set_xlabel("Memory size of include [KB]")
@@ -55,6 +56,6 @@ ax.set_ylabel("Compile time increase [seconds]")
 ax.spines["top"].set_visible(False)
 ax.spines["right"].set_visible(False)
 ax.set_ylim(0, )
-ax.grid(axis='x')
+ax.grid()
 fig.tight_layout()
 fig.savefig("compile_times.png")
